e86d58a9dee10660b53e95b4791a8653cce88d23
manuel1231234 <a23310403@ceti.mx>
2024-10-31 01:53:50 +0000

    clase 4

--- makefile
+++ makefile
@@ -1,5 +1,12 @@
 run : bin/programa
 	./bin/programa
 
-bin/programa : main.cpp include/*.hpp
-	g++ main.cpp -Iinclude -o bin/programa
+bin/programa : src/main.cpp include/*.hpp
+	g++ src/main.cpp -Iinclude -o bin/programa
+
+runTest : bin/test
+	./bin/test
+
+bin/test : src/test.cpp include/*.hpp
+	g++ src/test.cpp -Iinclude -o bin/test -std=c++2a -lftxui-component -lftxui-dom -lftxui-screen
+
---
+++ src/test.cpp
@@ -0,0 +1,41 @@
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
+#include <ftxui/screen/terminal.hpp>
+#include <iostream>
+#include <thread>
+
+using namespace std;
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+    std::string reset_position;
+    int frame = 0;
+    
+
+    
+    while (true)
+    {
+
+    auto can = Canvas(200,200);
+    can.DrawPointLine(35 + frame,70,100 + frame,70);
+    can.DrawPointCircle(25 + frame,70,10);
+    can.DrawPointCircle(110 + frame,70,10);
+    can.DrawPointLine(5 + frame,45,5 + frame,70);
+    can.DrawPointLine(5 + frame,70, 14 + frame,70);
+    can.DrawPointLine(5 + frame,45, 30 + frame,45);
+    can.DrawPointLine(100 + frame,45, 140 + frame,45);
+
+    Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
+    Element lienzo = bgcolor(Color::White, canvas(&can));
+    Render(pantalla, lienzo);
+    std::cout << reset_position;
+    pantalla.Print();
+    reset_position = pantalla.ResetPosition(true);
+    this_thread::sleep_for(0.1s);
+    frame++;
+    }
+
+    return 0;
+}
